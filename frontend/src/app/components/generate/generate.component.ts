1d8ea77d8c01cf7b7b1b0bff24cad2a6cbc18acd
Benedikt Schwering <benedikt.schwering@flender.com>
2023-05-08 10:49:43 +0200

    add generate component

--- frontend/src/app/components/generate/generate.component.ts
+++ frontend/src/app/components/generate/generate.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import html2canvas from 'html2canvas';
 import { Observable } from 'rxjs';
 import { CodeService } from 'src/app/services/code/code.service';
 import { LaneService } from 'src/app/services/lane/lane.service';
+import { Lane } from 'src/app/types/lane.type';
 
 @Component({
   selector: 'app-generate',
@@ -10,9 +12,13 @@ import { LaneService } from 'src/app/services/lane/lane.service';
 })
 export class GenerateComponent implements OnInit {
 
-  // lanes$!: Observable<Lane>;
+  lanes$!: Observable<Lane[]>;
+
+  lane!: Lane;
 
   url!: string;
+
+  png!: string;
 
   constructor(
     private laneService: LaneService,
@@ -20,11 +26,15 @@ export class GenerateComponent implements OnInit {
   ) { }
 
   ngOnInit(): void {
-    this.url = 'http://localhost:4200/test/123aksfdhaksdjhfasdafsdfasdfasdf';
+    this.getLanes();
   }
 
   getLanes() {
-    
+    this.lanes$ = this.laneService.getLanes();
+  }
+
+  generateCode() {
+    this.url = this.codeService.generateUrl(this.lane);
   }
 
 }
