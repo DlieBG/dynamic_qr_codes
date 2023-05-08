86ee1032e097815e3759ff1d9df9d6fcf63617c7
Benedikt Schwering <benedikt.schwering@flender.com>
2023-05-08 16:13:22 +0200

    add frontend lookup wip

--- frontend/src/app/components/lookup/lookup-information/lookup-information.component.ts
+++ frontend/src/app/components/lookup/lookup-information/lookup-information.component.ts
@@ -1,10 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Observable } from 'rxjs';
+import { LaneService } from 'src/app/services/lane/lane.service';
+import { UniquePartService } from 'src/app/services/unique-part/unique-part.service';
+import { Lane } from 'src/app/types/lane.type';
+import { UniquePart } from 'src/app/types/unique-part.type';
 
 @Component({
   selector: 'app-lookup-information',
   templateUrl: './lookup-information.component.html',
   styleUrls: ['./lookup-information.component.scss']
 })
-export class LookupInformationComponent {
+export class LookupInformationComponent implements OnInit {
+
+  lane$!: Observable<Lane>;
+  uniquePart$!: Observable<UniquePart>;
+
+  constructor(
+    private route: ActivatedRoute,
+    private laneService: LaneService,
+    private uniquePartService: UniquePartService
+  ) { }
+
+  ngOnInit(): void {
+    this.route.params.subscribe(
+      (params) => {
+        this.getLane(params['lane_id']);
+        this.getUniquePart(params['unique_part_id']);
+      }
+    );
+  }
+
+  getLane(id: string) {
+    this.lane$ = this.laneService.getLane(id);
+  }
+
+  getUniquePart(id: string) {
+    this.uniquePart$ = this.uniquePartService.getUniquePart(id);
+  }
 
 }
